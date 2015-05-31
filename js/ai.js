5053c22a6b6bf9bb0bce7f35c5f80556341db7fe
Jacsn <jacsn47@gmail.com>
2015-05-31 16:53:32 -0500

    Made AI stop when General Mean is destroyed.

--- js/ai.js
+++ js/ai.js
@@ -46,62 +46,68 @@ function findIntercept(a, b, acceleration){
 }
 
 function starCaptainAI(){
-	var me = starcaptain;
-	var enemy = generalmean;
+	if(generalmean.hp > 0)
+	{
+		var me = starcaptain;
+		var enemy = generalmean;
 
-	var attackSpeed = 35;
+		var attackSpeed = 35;
 
-	var intercept = findIntercept(me, enemy, SC_ACCEL);
-	me.angle = intercept.angle + Math.PI/2;
+		var intercept = findIntercept(me, enemy, SC_ACCEL);
+		me.angle = intercept.angle + Math.PI/2;
 
-	window.relativeSpeed = me.velocity.sub(enemy.velocity).length();
-	window.slowDistance = .5*(Math.pow(relativeSpeed, 2) - Math.pow(attackSpeed, 2)) / SC_ACCEL;
-	window.intercept = intercept;
-	window.distance = me.pos.sub(enemy.pos).length();
-	if(slowDistance > intercept.distance && generalmean.velocity.length() < starcaptain.velocity.length()){
-		me.angle = Math.atan2(me.velocity.y, me.velocity.x) + Math.PI + Math.PI/2;
-	}
+		window.relativeSpeed = me.velocity.sub(enemy.velocity).length();
+		window.slowDistance = .5*(Math.pow(relativeSpeed, 2) - Math.pow(attackSpeed, 2)) / SC_ACCEL;
+		window.intercept = intercept;
+		window.distance = me.pos.sub(enemy.pos).length();
+		if(slowDistance > intercept.distance && generalmean.velocity.length() < starcaptain.velocity.length()){
+			me.angle = Math.atan2(me.velocity.y, me.velocity.x) + Math.PI + Math.PI/2;
+		}
 
-	if(guntimer < 0){
-		guntimer = curframe;
-	}
+		if(guntimer < 0){
+			guntimer = curframe;
+		}
 
-	if(distance > 300){
-		me.thrusting = true;
-		var angle = me.angle - Math.PI / 2;
-		var speed = SC_ACCEL;
-		var vx = Math.cos(angle) * speed;
-		var vy = Math.sin(angle) * speed;
-		me.velocity.set(me.velocity.add(new Vector(vx, vy)));
-	}else{
-		me.thrusting = false;
+		if(distance > 300){
+			me.thrusting = true;
+			var angle = me.angle - Math.PI / 2;
+			var speed = SC_ACCEL;
+			var vx = Math.cos(angle) * speed;
+			var vy = Math.sin(angle) * speed;
+			me.velocity.set(me.velocity.add(new Vector(vx, vy)));
+		}else{
+			me.thrusting = false;
+		}
 	}
 }
 
 function generalMeanAI(){
-	var me = generalmean;
-	var enemy = starcaptain;
+	if(generalmean.hp > 0)
+	{
+		var me = generalmean;
+		var enemy = starcaptain;
 
-	var target;
-	if(pluto.isOrbitStable()){
-		target = pluto;
-	}else if(jupiter.isOrbitStable()){
-		target = jupiter;
-	}else if(venus.isOrbitStable()){
-		target = venus;
-	}else if(mercury.isOrbitStable()){
-		target = mercury;
-	}else{
-		target = bouncyplanet;
+		var target;
+		if(pluto.isOrbitStable()){
+			target = pluto;
+		}else if(jupiter.isOrbitStable()){
+			target = jupiter;
+		}else if(venus.isOrbitStable()){
+			target = venus;
+		}else if(mercury.isOrbitStable()){
+			target = mercury;
+		}else{
+			target = bouncyplanet;
+		}
+
+		me.angle = findIntercept(me, target, GM_ACCEL).angle + Math.PI/2;
+		me.thrusting = true;
+		var angle = me.angle - Math.PI / 2;
+		var speed = GM_ACCEL;
+		var vx = Math.cos(angle) * speed;
+		var vy = Math.sin(angle) * speed;
+		me.velocity.set(me.velocity.add(new Vector(vx, vy)));
 	}
-
-	me.angle = findIntercept(me, target, GM_ACCEL).angle + Math.PI/2;
-	me.thrusting = true;
-	var angle = me.angle - Math.PI / 2;
-	var speed = GM_ACCEL;
-	var vx = Math.cos(angle) * speed;
-	var vy = Math.sin(angle) * speed;
-	me.velocity.set(me.velocity.add(new Vector(vx, vy)));
 }
 
 function updateAI(){
